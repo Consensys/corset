2653e62d0e5b736556b2537a795eae3a8cadf1c3
Franklin Delehelle <franklin.delehelle@odena.eu>
2022-09-05 19:00:24 +0200

    Split files

--- src/compiler.rs
+++ src/compiler.rs
@@ -3,6 +3,8 @@ use eyre::*;
 
 mod generator;
 mod parser;
+mod define;
+
 
 pub fn compile(sources: &[(&str, &str)]) -> Result<ConstraintsSet> {
     generator::compile(sources)
--- src/compiler/generator.rs
+++ src/compiler/generator.rs
@@ -1,137 +1,12 @@
 use eyre::*;
 use std::cell::RefCell;
-use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
 use std::rc::Rc;
 
+use crate::compiler::define;
+use crate::compiler::define::SymbolTable;
 use crate::compiler::parser::*;
 use crate::utils::*;
-
-lazy_static::lazy_static! {
-    static ref BUILTINS: HashMap<&'static str, Function> = maplit::hashmap!{
-        "defconstraint" => Function {
-            name: "defconstring".into(),
-            class: FunctionClass::SpecialForm(Form::Defconstraint),
-        },
-
-        "defun" => Function {
-            name: "defun".into(),
-            class: FunctionClass::SpecialForm(Form::Defun),
-        },
-
-        "defunalias" => Function {
-            name: "defunalias".into(),
-            class: FunctionClass::SpecialForm(Form::Defunalias),
-        },
-
-        "defalias" => Function {
-            name: "defalias".into(),
-            class: FunctionClass::SpecialForm(Form::Defalias),
-        },
-
-        "defconst" => Function {
-            name: "defconst".into(),
-            class: FunctionClass::SpecialForm(Form::Defconst),
-        },
-
-        "nth" => Function {
-            name: "nth".into(),
-            class: FunctionClass::Builtin(Builtin::Nth),
-        },
-
-        "for" => Function {
-            name: "for".into(),
-            class: FunctionClass::SpecialForm(Form::For),
-        },
-
-
-        // monadic
-        "inv" => Function{
-            name: "inv".into(),
-            class: FunctionClass::Builtin(Builtin::Inv)
-        },
-        "neg" => Function{
-            name: "neg".into(),
-            class: FunctionClass::Builtin(Builtin::Neg)
-        },
-
-        // Dyadic
-        "if-zero" => Function{
-            name: "if-zero".into(),
-            class: FunctionClass::Builtin(Builtin::IfZero),
-        },
-
-        "shift" => Function{
-            name: "shift".into(),
-            class: FunctionClass::Builtin(Builtin::Shift),
-        },
-
-
-        // polyadic
-        "add" => Function{
-            name: "add".into(),
-            class: FunctionClass::Builtin(Builtin::Add)
-        },
-        "+" => Function {
-            name: "+".into(),
-            class: FunctionClass::Alias("add".into())
-        },
-
-
-        "mul" => Function{
-            name: "mul".into(),
-            class: FunctionClass::Builtin(Builtin::Mul)
-        },
-        "*" => Function {
-            name: "*".into(),
-            class: FunctionClass::Alias("mul".into())
-        },
-        "and" => Function {
-            name: "and".into(),
-            class: FunctionClass::Alias("mul".into())
-        },
-
-        "sub" => Function{
-            name: "mul".into(),
-            class: FunctionClass::Builtin(Builtin::Sub,)
-        },
-        "-" => Function {
-            name: "sub".into(),
-            class: FunctionClass::Alias("sub".into())
-        },
-        "eq" => Function {
-            name: "sub".into(),
-            class: FunctionClass::Alias("sub".into())
-        },
-        "=" => Function {
-            name: "sub".into(),
-            class: FunctionClass::Alias("sub".into())
-        },
-
-        "begin" => Function{name: "begin".into(), class: FunctionClass::Builtin(Builtin::Begin)},
-
-        // Special form for now, see later if implementing map...
-        "branch-if-zero" => Function {
-            name:"branch-if-zero".into(),
-            class: FunctionClass::Builtin(Builtin::BranchIfZero)
-        },
-
-        "branch-if-zero-else" => Function {
-            name:"branch-if-zero-else".into(),
-            class: FunctionClass::Builtin(Builtin::BranchIfZeroElse)
-        },
-
-        "branch-if-not-zero" => Function {
-            name:"branch-if-not-zero".into(),
-            class: FunctionClass::Builtin(Builtin::BranchIfNotZero)
-        },
-
-        "branch-if-not-zero-else" => Function {
-            name:"branch-if-not-zero-else".into(),
-            class: FunctionClass::Builtin(Builtin::BranchIfNotZeroElse)
-        },
-    };
-}
 
 trait FuncVerifier<T> {
     fn arity(&self) -> Arity;
@@ -190,7 +65,7 @@ impl Arity {
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
-enum Form {
+pub enum Form {
     Defconstraint,
     Defun,
     Defalias,
@@ -361,12 +236,12 @@ impl FuncVerifier<Constraint> for Builtin {
 }
 
 #[derive(Debug, Clone)]
-struct Function {
-    name: String,
-    class: FunctionClass,
+pub struct Function {
+    pub name: String,
+    pub class: FunctionClass,
 }
 #[derive(Debug, Clone)]
-enum FunctionClass {
+pub enum FunctionClass {
     UserDefined(Defined),
     SpecialForm(Form),
     Builtin(Builtin),
@@ -374,7 +249,7 @@ enum FunctionClass {
 }
 
 #[derive(Debug, Clone)]
-struct Defined {
+pub struct Defined {
     args: Vec<String>,
     body: AstNode,
 }
@@ -385,153 +260,6 @@ impl FuncVerifier<Constraint> for Defined {
 
     fn validate_types(&self, _args: &[Constraint]) -> Result<()> {
         Ok(())
-    }
-}
-
-#[derive(Debug)]
-enum Symbol {
-    Alias(String),
-    Final(Constraint),
-}
-#[derive(Debug)]
-struct SymbolTable {
-    parent: Option<Rc<RefCell<SymbolTable>>>,
-    constraints: HashSet<String>,
-    funcs: HashMap<String, Function>,
-    symbols: HashMap<String, Symbol>,
-}
-impl SymbolTable {
-    fn new_root() -> SymbolTable {
-        SymbolTable {
-            funcs: BUILTINS
-                .iter()
-                .map(|(k, v)| (k.to_string(), v.clone()))
-                .collect(),
-            symbols: Default::default(),
-            parent: None,
-            constraints: Default::default(),
-        }
-    }
-
-    fn derived(parent: Rc<RefCell<SymbolTable>>) -> Rc<RefCell<SymbolTable>> {
-        Rc::new(RefCell::new(SymbolTable {
-            funcs: Default::default(),
-            symbols: Default::default(),
-            constraints: Default::default(),
-            parent: Some(parent),
-        }))
-    }
-
-    fn _resolve_symbol(&self, name: &str, ax: &mut HashSet<String>) -> Result<Constraint> {
-        if ax.contains(name) {
-            Err(eyre!("Circular definitions found for {}", name))
-        } else {
-            ax.insert(name.into());
-            match self.symbols.get(name) {
-                Some(Symbol::Alias(name)) => self._resolve_symbol(name, ax),
-                Some(Symbol::Final(constraint)) => Ok(constraint.clone()),
-                None => self
-                    .parent
-                    .as_ref()
-                    .map_or(Err(eyre!("Column `{}` unknown", name)), |parent| {
-                        parent.borrow().resolve_symbol(name)
-                    }),
-            }
-        }
-    }
-
-    fn _resolve_function(&self, name: &str, ax: &mut HashSet<String>) -> Result<Function> {
-        if ax.contains(name) {
-            Err(eyre!("Circular definitions found for {}", name))
-        } else {
-            ax.insert(name.into());
-            match self.funcs.get(name) {
-                Some(Function {
-                    class: FunctionClass::Alias(ref name),
-                    ..
-                }) => self._resolve_function(name, ax),
-                Some(f) => Ok(f.to_owned()),
-                None => self
-                    .parent
-                    .as_ref()
-                    .map_or(Err(eyre!("Function `{}` unknown", name)), |parent| {
-                        parent.borrow().resolve_function(name)
-                    }),
-            }
-        }
-    }
-
-    fn insert_constraint(&mut self, name: &str) -> Result<()> {
-        self.constraints
-            .insert(name.into())
-            .then(|| ())
-            .ok_or_else(|| eyre!("Constraint `{}` already defined", name))
-    }
-
-    fn insert_symbol(&mut self, symbol: &str, constraint: Constraint) -> Result<()> {
-        if self.symbols.contains_key(symbol) {
-            Err(anyhow!("column `{}` already exists", symbol))
-        } else {
-            self.symbols
-                .insert(symbol.into(), Symbol::Final(constraint));
-            Ok(())
-        }
-    }
-
-    fn insert_func(&mut self, f: Function) -> Result<()> {
-        if self.funcs.contains_key(&f.name) {
-            Err(anyhow!("function `{}` already defined", &f.name))
-        } else {
-            self.funcs.insert(f.name.clone(), f);
-            Ok(())
-        }
-    }
-
-    fn insert_alias(&mut self, from: &str, to: &str) -> Result<()> {
-        if self.symbols.contains_key(from) {
-            Err(anyhow!("`{}` already exists", from))
-        } else {
-            self.symbols.insert(from.into(), Symbol::Alias(to.into()));
-            Ok(())
-        }
-    }
-
-    fn insert_funalias(&mut self, from: &str, to: &str) -> Result<()> {
-        if self.symbols.contains_key(from) {
-            Err(anyhow!(
-                "`{}` already exists: {} -> {:?}",
-                from,
-                from,
-                self.symbols[from]
-            ))
-        } else {
-            self.funcs.insert(
-                from.into(),
-                Function {
-                    name: from.into(),
-                    class: FunctionClass::Alias(to.into()),
-                },
-            );
-            Ok(())
-        }
-    }
-
-    fn resolve_symbol(&self, name: &str) -> Result<Constraint> {
-        self._resolve_symbol(name, &mut HashSet::new())
-    }
-
-    fn resolve_function(&self, name: &str) -> Result<Function> {
-        self._resolve_function(name, &mut HashSet::new())
-    }
-
-    fn insert_constant(&mut self, name: &str, value: i32) -> Result<()> {
-        if self.symbols.contains_key(name) {
-            Err(anyhow!("`{}` already exists", name))
-        } else {
-            self.symbols
-                .insert(name.into(), Symbol::Final(Constraint::Const(value)));
-            Ok(())
-        }
     }
 }
 
@@ -891,20 +619,21 @@ enum Pass {
     Compilation,
 }
 pub fn compile(sources: &[(&str, &str)]) -> Result<ConstraintsSet> {
-    let table = Rc::new(RefCell::new(SymbolTable::new_root()));
     let mut asts = vec![];
+    let ctx = Rc::new(RefCell::new(SymbolTable::new_root()));
 
     for (name, content) in sources.iter() {
         let ast = parse(content).with_context(|| eyre!("parsing `{}`", name))?;
-        let _ = build_constraints(&ast, table.clone(), Pass::Definition)
-            .with_context(|| eyre!("parsing top-level definitions in `{}`", name))?;
+        define::parse(ctx.clone())
+            .with_context(|| eyre!("assembling definitions in `{}`", name))?;
+        let _ = build_constraints(&ast, ctx.clone(), Pass::Definition);
         asts.push((name, ast));
     }
 
     let constraints = asts
         .into_iter()
         .map(|(name, ast)| {
-            build_constraints(&ast, table.clone(), Pass::Compilation)
+            build_constraints(&ast, ctx.clone(), Pass::Compilation)
                 .with_context(|| eyre!("compiling constraints in `{}`", name))
         })
         .collect::<Result<Vec<_>>>()?
