104e069deb052c2702fe4d4b9e6f1822987b7118
Franklin Delehelle <franklin.delehelle@odena.eu>
2022-12-29 20:07:48 +0100

    doc: Update README

--- src/tests.rs
+++ src/tests.rs
@@ -117,3 +117,61 @@ fn huge_const() {
     ))
     .is_ok());
 }
+
+#[test]
+fn array_ok() {
+    let inputs = vec![
+        ("stdlib", include_str!("stdlib.lisp")),
+        (
+            "arrays",
+            "
+(defcolumns A (B :ARRAY[3]) (C :ARRAY [5:8]) (D :ARRAY [8:32:5]) (Q :ARRAY [5]))
+(defalias qq D)
+
+(defcolumns
+  (EXAMPLE1 :ARRAY[2])       ;; EXAMPLE1 is defined over {1, 2}
+  (EXAMPLE2 :ARRAY[4:7])     ;; EXAMPLE2 is defined over {4, 5, 6, 7}
+  (EXAMPLE3 :ARRAY[2:10:2])  ;; EXAMPLE3 is defined over {2, 4, 6, 8, 10}
+  (EXAMPLE4 :ARRAY{1 6 8}))  ;; EXAMPLE4 is defined over {1, 6, 8}
+
+(defconstraint asdf () (eq (nth B 3) (nth C 8)))
+(defconstraint fdsa () (eq A (nth D 28)))
+(defconstraint fdsa2 () (eq A (nth qq 28)))
+",
+        ),
+    ];
+
+    assert!(dbg!(compiler::make(
+        inputs.as_slice(),
+        &CompileSettings {
+            debug: false,
+            allow_dups: false
+        }
+    ))
+    .is_ok());
+}
+
+#[test]
+fn array_ko() {
+    let inputs = vec![
+        ("stdlib", include_str!("stdlib.lisp")),
+        (
+            "arrays",
+            "
+  (defcolumns (EXAMPLE4{1 6 8}))
+
+  (defconstraint will-fail ()
+    (nth EXAMPLE4 2)) ;; 2 ∉ {1, 6, 8}
+",
+        ),
+    ];
+
+    assert!(dbg!(compiler::make(
+        inputs.as_slice(),
+        &CompileSettings {
+            debug: false,
+            allow_dups: false
+        }
+    ))
+    .is_err());
+}
