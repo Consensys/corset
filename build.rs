4709cdb26c80117d315aab6892c296530cb65405
Franklin Delehelle <franklin.delehelle@odena.eu>
2023-01-16 18:37:57 +0100

    fix: do not crash at build if git fails

--- build.rs
+++ build.rs
@@ -1,13 +1,11 @@
 use std::process::Command;
 fn main() {
-    let output = Command::new("git")
+    let git_hash = Command::new("git")
         .args(&["rev-parse", "HEAD"])
         .output()
-        .expect("can not run git");
-    let git_hash = String::from_utf8(output.stdout)
-        .unwrap()
-        .chars()
-        .take(7)
-        .collect::<String>();
+        .ok()
+        .and_then(|o| String::from_utf8(o.stdout).ok())
+        .map(|s| s.chars().take(7).collect::<String>())
+        .unwrap_or("UNKNW".into());
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 }
