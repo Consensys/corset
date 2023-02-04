e3580d54e8c0a02e87ef45dd8be1c6f35eb0dbf8
Franklin Delehelle <franklin.delehelle@odena.eu>
2023-02-04 21:32:59 +0100

    refactor: introduce testing helpers

--- src/tests.rs
+++ src/tests.rs
@@ -1,12 +1,8 @@
 use crate::compiler::{self, CompileSettings};
 use anyhow::*;
 
-#[test]
-fn types_declaration() -> Result<()> {
-    let inputs = vec![
-        ("stdlib", include_str!("stdlib.lisp")),
-        ("types", include_str!("../tests/types.lisp")),
-    ];
+fn make(name: &str, source: &str) -> Result<()> {
+    let inputs = vec![("stdlib", include_str!("stdlib.lisp")), (name, source)];
 
     compiler::make(
         inputs.as_slice(),
@@ -14,117 +10,70 @@ fn types_declaration() -> Result<()> {
             debug: false,
             allow_dups: true,
         },
-    )?;
+    )
+    .map(|_| ())
+}
 
-    Ok(())
+fn must_run(name: &str, source: &str) {
+    let r = make(name, source);
+    assert!(r.is_ok());
+}
+
+fn must_fail(name: &str, source: &str) {
+    let r = make(name, source);
+    assert!(r.is_err());
+}
+
+#[test]
+fn types_declaration() -> Result<()> {
+    make("type", include_str!("../tests/types.lisp"))
 }
 
 #[test]
 fn defun_too_many_args() {
-    let inputs =
-        vec![
-       ("stdlib", include_str!("stdlib.lisp")), (
+    must_fail(
         "too-big",
         "(defcolumns X Y) (defun (f A B) (eq A 3) (eq C 5)) (defconstraint asdf () (f X Y))",
-    )];
-
-    assert!(compiler::make(
-        inputs.as_slice(),
-        &CompileSettings {
-            debug: false,
-            allow_dups: false
-        }
-    )
-    .is_err());
+    );
 }
 
 #[test]
 fn defpure_ok() {
-    let inputs = vec![
-        ("stdlib", include_str!("stdlib.lisp")),
-        (
-            "defpurefun",
-            "(defcolumns x y) (defpurefun (f a b) (eq a b))",
-        ),
-    ];
-
-    assert!(compiler::make(
-        inputs.as_slice(),
-        &CompileSettings {
-            debug: false,
-            allow_dups: false,
-        },
-    )
-    .is_ok());
+    must_run(
+        "defpurefun",
+        "(defcolumns x y) (defpurefun (f a b) (eq a b))",
+    );
 }
 
 #[test]
-fn defpure_ok_const() -> Result<()> {
-    let inputs = vec![
-        ("stdlib", include_str!("stdlib.lisp")),
-        (
-            "defpurefun",
-            "(defconst A 123) (defcolumns x) (defpurefun (f a) (eq a A)) (defconstraint asdf () (f x))",
-        ),
-    ];
-
-    compiler::make(
-        inputs.as_slice(),
-        &CompileSettings {
-            debug: false,
-            allow_dups: false,
-        },
-    )?;
-    Ok(())
+fn defpure_ok_const() {
+    must_run(
+        "defpurefun",
+        "(defconst A 123) (defcolumns x) (defpurefun (f a) (eq a A)) (defconstraint asdf () (f x))",
+    );
 }
 
 #[test]
 fn defpure_ko() {
-    let inputs = vec![
-        ("stdlib", include_str!("stdlib.lisp")),
-        (
+    must_fail(
             "defpurefun",
             "(defcolumns X Y Z) (defpurefun (f A B) (begin (eq A 3) (eq B Z))) (defconstraint asdf () (f X Y))",
-        ),
-    ];
-
-    assert!(dbg!(compiler::make(
-        inputs.as_slice(),
-        &CompileSettings {
-            debug: false,
-            allow_dups: false
-        }
-    ))
-    .is_err());
+        )
 }
 
 #[test]
 fn huge_const() {
-    let inputs = vec![
-        ("stdlib", include_str!("stdlib.lisp")),
-        (
-            "hugeconstant",
-            "(defconst A 340282366920938463463374607431768211456)",
-        ),
-    ];
-
-    assert!(dbg!(compiler::make(
-        inputs.as_slice(),
-        &CompileSettings {
-            debug: false,
-            allow_dups: false
-        }
-    ))
-    .is_ok());
+    must_run(
+        "hugeconstant",
+        "(defconst A 340282366920938463463374607431768211456)",
+    );
 }
 
 #[test]
 fn array_ok() {
-    let inputs = vec![
-        ("stdlib", include_str!("stdlib.lisp")),
-        (
-            "arrays",
-            "
+    must_run(
+        "arrays",
+        "
 (defcolumns A (B :ARRAY[3]) (C :ARRAY [5:8]) (D :ARRAY [8:32:5]) (Q :ARRAY [5]))
 (defalias qq D)
 
@@ -138,54 +87,19 @@ fn array_ok() {
 (defconstraint fdsa () (eq A (nth D 28)))
 (defconstraint fdsa2 () (eq A (nth qq 28)))
 ",
-        ),
-    ];
-
-    assert!(dbg!(compiler::make(
-        inputs.as_slice(),
-        &CompileSettings {
-            debug: false,
-            allow_dups: false
-        }
-    ))
-    .is_ok());
+    );
 }
 
 #[test]
 fn array_ko() {
-    let inputs = vec![
-        ("stdlib", include_str!("stdlib.lisp")),
-        (
-            "arrays",
-            "
-  (defcolumns (EXAMPLE4{1 6 8}))
-
-  (defconstraint will-fail ()
-    (nth EXAMPLE4 2)) ;; 2 ∉ {1, 6, 8}
-",
-        ),
-    ];
-
-    assert!(dbg!(compiler::make(
-        inputs.as_slice(),
-        &CompileSettings {
-            debug: false,
-            allow_dups: false
-        }
-    ))
-    .is_err());
+    must_fail(
+        "arrays",
+        "(defcolumns (EXAMPLE4{1 6 8}))
+         (defconstraint will-fail () (nth EXAMPLE4 2))",
+    );
 }
 
 #[test]
 fn prime_in_name() {
-    let inputs = vec![("prime", "(defcolumns A B C A' B' C')")];
-
-    assert!(dbg!(compiler::make(
-        inputs.as_slice(),
-        &CompileSettings {
-            debug: false,
-            allow_dups: false
-        }
-    ))
-    .is_ok());
+    must_run("quotes in names", "(defcolumns A B C A' B' C')");
 }
