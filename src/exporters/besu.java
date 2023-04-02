ddc74eb19533de19af2e84f0298a2aafb9a1c815
Daniel Lehrner <daniel.lehrner@consensys.net>
2023-04-02 14:49:07 +0200

    Some fixes for the temple, removed stamp because it is not needed anymore

--- src/exporters/besu.java
+++ src/exporters/besu.java
@@ -1,5 +1,5 @@
 /*
- * Copyright ConsenSys AG.
+ * Copyright ConsenSys Software Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -9,8 +9,6 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
  */
 package net.consensys.zktracer.module.{{ module }};
 
@@ -21,50 +19,45 @@ import java.util.ArrayList;
 import java.util.List;
 import net.consensys.zktracer.bytes.UnsignedByte;
 
-@JsonPropertyOrder({"Trace", "Stamp"})
 @SuppressWarnings("unused")
-public record {{ module_prefix }}Trace(@JsonProperty("Trace") Trace trace, @JsonProperty("Stamp") int stamp) {
-    {{#each constants}}
-    public static final BigInteger {{ this.name }} = BigInteger("{{ this.value }}");
-    {{/each}}
+public record {{ module_prefix }}Trace(@JsonProperty("Trace") Trace trace) {
+  {{#each constants}}
+  public static final BigInteger {{ this.name }} = new BigInteger("{{ this.value }}");
+  {{/each}}
 
-    @JsonPropertyOrder({
-    {{#each columns}}
-        "{{ this.corset_name }}",
-    {{/each}}
-    })
+  @JsonPropertyOrder({
+  {{#each columns}}
+    "{{ this.corset_name }}",
+  {{/each}}
+  })
+  @SuppressWarnings("unused")
+  private record Trace(
+  {{#each columns}}
+    @JsonProperty("{{ this.corset_name }}") List<{{ this.tupe }}> {{ this.corset_name }}{{#unless @last}},{{/unless}}
+  {{/each}}
+  ) {}
 
-    @SuppressWarnings("unused")
-    public record Trace(
-    {{#each columns}}
-        @JsonProperty("{{ this.corset_name }}") List<{{ this.tupe }}> {{ this.corset_name }},
-    {{/each}}
-    )
-
-    public static class Builder {
-    {{#each columns}}
-        private final List<{{ this.tupe }}> {{ this.java_name }} = new ArrayList<>();
-    {{/each}}
-    }
+  public static class Builder {
+  {{#each columns}}
+    private final List<{{ this.tupe }}> {{ this.java_name }} = new ArrayList<>();
+  {{/each}}
 
     private Builder() {}
 
-    public Builder {
-    {{#each columns}}
-        {{ this.appender_name }}(final {{ this.tupe }} b) {
-            {{ this.java_name }}.add(b);
-            return this;
-        }
-    {{/each}}
+  {{#each columns}}
+    public Builder {{ this.appender_name }}(final {{ this.tupe }} b) {
+      {{ this.java_name }}.add(b);
+      return this;
     }
 
+  {{/each}}
     public {{ module_prefix }}Trace build() {
-        return new {{ module_prefix }}Trace(
-            new Trace(
-            {{#each columns}}
-                {{ this.java_name }},
-            {{/each}}
-            )
+      return new {{ module_prefix }}Trace(
+        new Trace(
+        {{#each columns}}
+          {{ this.java_name }}{{#unless @last}},{{/unless}}{{#if @last}}){{/if}}
+        {{/each}}
         );
     }
+  }
 }
