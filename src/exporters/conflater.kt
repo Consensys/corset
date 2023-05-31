558ddbded4ff39478eb800d96743ff9430a33b2e
Franklin Delehelle <franklin.delehelle@odena.eu>
2023-05-31 12:01:02 +0200

    fix: JSON export in conflated traces

--- src/exporters/conflater.kt
+++ src/exporters/conflater.kt
@@ -13,7 +13,7 @@ val MODULES = listOf(
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class {{ this.klass_name }}(
   {{ #each this.columns }}
-  @JsonProperty("{{ this.json_name }}") val {{ this.safe_name }}: MutableList<String> = arrayListOf(),
+  @get:JsonProperty("{{ this.json_name }}") val {{ this.safe_name }}: MutableList<String> = arrayListOf(),
   {{ /each }}
 )
 {{ /each }}
