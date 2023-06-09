2fb0e5c3ead2184b9bbf3b7ca864368d6a579db8
Franklin Delehelle <franklin.delehelle@odena.eu>
2023-06-09 20:12:38 +0100

    ffi: add an error to string converter

--- src/lib.rs
+++ src/lib.rs
@@ -30,13 +30,54 @@ mod utils;
 
 type Corset = ConstraintSet;
 
-pub const ERR_NOT_AN_USIZE: i32 = 1;
-pub const ERR_COMPUTE_TRACE_FAILED: i32 = 2;
-pub const ERR_COLUMN_NAME_NOT_FOUND: i32 = 3;
-pub const ERR_COULD_NOT_INITIALIZE_RAYON: i32 = 4;
-pub const ERR_COLUMN_ID_NOT_FOUND: i32 = 5;
-pub const ERR_INVALID_ZKEVM_FILE: i32 = 6;
-pub const ERR_CHECK_FAILED: i32 = 7;
+#[derive(Copy, Clone)]
+#[repr(i32)]
+enum CorsetError {
+    NotAnUsize = 1,
+    ComputeTraceFailed,
+    ColumnNameNotFound,
+    ColumnIdNotFound,
+    InitializingRayon,
+    InvalidZkEvmFile,
+    CheckFailed,
+    EmptyTrace,
+    NotAnError,
+}
+impl From<i32> for CorsetError {
+    fn from(x: i32) -> Self {
+        match x {
+            a if a == CorsetError::NotAnUsize as i32 => CorsetError::NotAnUsize,
+            a if a == CorsetError::ComputeTraceFailed as i32 => CorsetError::ComputeTraceFailed,
+            a if a == CorsetError::ColumnNameNotFound as i32 => CorsetError::ColumnNameNotFound,
+            a if a == CorsetError::ColumnIdNotFound as i32 => CorsetError::ColumnIdNotFound,
+            a if a == CorsetError::InitializingRayon as i32 => CorsetError::InitializingRayon,
+            a if a == CorsetError::InvalidZkEvmFile as i32 => CorsetError::InvalidZkEvmFile,
+            a if a == CorsetError::CheckFailed as i32 => CorsetError::CheckFailed,
+            a if a == CorsetError::EmptyTrace as i32 => CorsetError::EmptyTrace,
+            _ => CorsetError::NotAnError,
+        }
+    }
+}
+impl From<CorsetError> for Errno {
+    fn from(e: CorsetError) -> Self {
+        Errno(e as i32)
+    }
+}
+impl std::fmt::Display for CorsetError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            CorsetError::NotAnUsize => write!(f, "could not convert to usize"),
+            CorsetError::ComputeTraceFailed => write!(f, "failed to expand the trace"),
+            CorsetError::ColumnNameNotFound => write!(f, "column name not found"),
+            CorsetError::ColumnIdNotFound => write!(f, "column ID not found"),
+            CorsetError::InitializingRayon => write!(f, "failed to initialize rayon"),
+            CorsetError::InvalidZkEvmFile => write!(f, "invalid zkEVM constraints file"),
+            CorsetError::CheckFailed => write!(f, "the trace does not satisfy the constraints"),
+            CorsetError::EmptyTrace => write!(f, "refusing to process an empty trace"),
+            CorsetError::NotAnError => write!(f, "this is not a valid Corset error"),
+        }
+    }
+}
 
 fn cstr_to_string<'a>(s: *const c_char) -> &'a str {
     let name = unsafe {
@@ -219,7 +260,7 @@ fn _compute_trace_from_file(
     fail_on_missing: bool,
 ) -> Result<Trace> {
     compute::compute_trace(tracefile, constraints, fail_on_missing)
-        .with_context(|| format!("while computing from `{}`", tracefile))?;
+        .with_context(|| format!("while computing from file `{}`", tracefile))?;
     Ok(Trace::from_constraints(constraints, convert_to_be))
 }
 
@@ -230,7 +271,7 @@ fn _compute_trace_from_str(
     fail_on_missing: bool,
 ) -> Result<Trace> {
     compute::compute_trace_str(tracestr.as_bytes(), constraints, fail_on_missing)
-        .with_context(|| format!("while computing from `{}`", tracestr))?;
+        .with_context(|| format!("while computing from string `{}`", tracestr))?;
     Ok(Trace::from_constraints(constraints, convert_to_be))
 }
 
@@ -244,7 +285,7 @@ pub extern "C" fn corset_from_file(zkevmfile: *const c_char) -> *mut Corset {
         }
         Err(e) => {
             eprintln!("{:?}", e);
-            set_errno(Errno(ERR_INVALID_ZKEVM_FILE));
+            set_errno(Errno(CorsetError::InvalidZkEvmFile as i32));
             std::ptr::null_mut()
         }
     }
@@ -260,7 +301,7 @@ pub extern "C" fn corset_from_string(zkevmstr: *const c_char) -> *mut Corset {
         }
         Err(e) => {
             eprintln!("{:?}", e);
-            set_errno(Errno(ERR_INVALID_ZKEVM_FILE));
+            set_errno(CorsetError::InvalidZkEvmFile.into());
             std::ptr::null_mut()
         }
     }
@@ -300,13 +341,13 @@ pub extern "C" fn trace_check(
         .num_threads(if let Result::Ok(t) = threads.try_into() {
             t
         } else {
-            set_errno(Errno(ERR_NOT_AN_USIZE));
+            set_errno(CorsetError::NotAnUsize.into());
             return false;
         })
         .build_global()
         .is_err()
     {
-        set_errno(Errno(ERR_COULD_NOT_INITIALIZE_RAYON));
+        set_errno(CorsetError::InitializingRayon.into());
         return false;
     }
 
@@ -317,7 +358,7 @@ pub extern "C" fn trace_check(
         Result::Ok(_) => true,
         Err(e) => {
             eprintln!("{e:?}");
-            set_errno(Errno(ERR_CHECK_FAILED));
+            set_errno(CorsetError::CheckFailed.into());
             false
         }
     }
@@ -328,13 +369,13 @@ fn init_rayon(threads: c_uint) -> Result<()> {
         .num_threads(if let Result::Ok(t) = threads.try_into() {
             t
         } else {
-            set_errno(Errno(ERR_NOT_AN_USIZE));
+            set_errno(CorsetError::NotAnUsize.into());
             bail!("not an usize");
         })
         .build_global()
         .is_err()
     {
-        set_errno(Errno(ERR_COULD_NOT_INITIALIZE_RAYON));
+        set_errno(CorsetError::InitializingRayon.into());
         bail!("unable to initialize rayon")
     }
     Ok(())
@@ -358,7 +399,7 @@ pub extern "C" fn trace_compute_from_file(
     match r {
         Err(e) => {
             eprintln!("{:?}", e);
-            set_errno(Errno(ERR_COMPUTE_TRACE_FAILED));
+            set_errno(CorsetError::ComputeTraceFailed.into());
             std::ptr::null_mut()
         }
         Result::Ok(x) => {
@@ -377,16 +418,22 @@ pub extern "C" fn trace_compute_from_string(
     fail_on_missing: bool,
 ) -> *mut Trace {
     if init_rayon(threads).is_err() {
+        set_errno(CorsetError::InitializingRayon.into());
         return std::ptr::null_mut();
     }
 
     let tracestr = cstr_to_string(tracestr);
+    if tracestr.is_empty() {
+        set_errno(CorsetError::EmptyTrace.into());
+        return std::ptr::null_mut();
+    }
+
     let constraints = Corset::mut_from_ptr(corset);
     let r = _compute_trace_from_str(constraints, tracestr, convert_to_be, fail_on_missing);
     match r {
         Err(e) => {
             eprintln!("{:?}", e);
-            set_errno(Errno(ERR_COMPUTE_TRACE_FAILED));
+            set_errno(CorsetError::ComputeTraceFailed.into());
             std::ptr::null_mut()
         }
         Result::Ok(x) => {
@@ -450,7 +497,7 @@ pub extern "C" fn trace_column_by_name(trace: *const Trace, name: *const c_char)
         trace_column_by_id(trace, i.try_into().unwrap())
     } else {
         let r = Default::default();
-        set_errno(Errno(ERR_COLUMN_NAME_NOT_FOUND));
+        set_errno(CorsetError::ColumnNameNotFound.into());
         r
     }
 }
@@ -464,7 +511,7 @@ pub extern "C" fn trace_column_by_id(trace: *const Trace, i: u32) -> ColumnData 
         c
     } else {
         let r = ColumnData::default();
-        set_errno(Errno(ERR_COLUMN_ID_NOT_FOUND));
+        set_errno(CorsetError::ColumnIdNotFound.into());
         return r;
     };
 
@@ -473,4 +520,10 @@ pub extern "C" fn trace_column_by_id(trace: *const Trace, i: u32) -> ColumnData 
         values: col.values.as_ptr(),
         values_len: col.values.len() as u64,
     }
+}
+
+#[no_mangle]
+pub extern "C" fn corset_err_to_string(err: i32) -> *mut c_char {
+    let err: CorsetError = err.into();
+    CString::new(err.to_string()).unwrap().into_raw()
 }
